4fb52288a406aaa14b1023870adb526ac6ea1f9f
Prashant Choudhary <prash.sastra1@gmail.com>
2023-02-16 22:52:57 +0530

    Okta Config Backend Security Configuration

---
+++ src/main/java/com/librarymanagement/restapis/codes/config/SecurityConfiguration.java
@@ -0,0 +1,38 @@
+package com.librarymanagement.restapis.codes.config;
+
+import com.okta.spring.boot.oauth.Okta;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.accept.ContentNegotiationStrategy;
+import org.springframework.web.accept.HeaderContentNegotiationStrategy;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        //Disable CSRF
+        http.cors().disable();
+
+        //Protect endpoints at /api/<type>/secure
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers("/api/books/secure")
+                        .authenticated())
+                .oauth2ResourceServer()
+                .jwt();
+
+        //Add Cors filter
+        http.cors();
+
+        //Add content negotiation strategy
+        http.setSharedObject(ContentNegotiationStrategy.class, new HeaderContentNegotiationStrategy());
+
+        //Force nonEmpty response body for 401
+        Okta.configureResourceServer401ResponseBody(http);
+
+        return http.build();
+    }
+}
